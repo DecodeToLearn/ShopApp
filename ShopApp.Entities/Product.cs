3e82d46543f89a84ea5d42bfd92ba41b8d5276eb
Yusuf Sengul <yusufsengul@Yusufs-iMac.local>
2022-10-27 16:33:12 +0300

    Add entities models in ShopApp.Entites layer

---
+++ ShopApp.Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopApp.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+
+        public List<ProductCategory> ProductCategories { get; set; }
+    }
+}
+
---
+++ ShopApp.Entities/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ShopApp.Entities
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
+
